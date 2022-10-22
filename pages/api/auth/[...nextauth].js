c7714c835039f0b30c188b02e055bda9481143d0
cichowlasp <cichowlas.p@gmail.com>
2022-10-22 20:17:32 +0200

    auth implementation

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,62 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import prisma from '../../../lib/prisma';
+
+export const authOptions = {
+	// Configure one or more authentication providers
+	secret: process.env.SECRET,
+	session: {
+		strategy: 'jwt',
+	},
+	providers: [
+		CredentialsProvider({
+			// The name to display on the sign in form (e.g. 'Sign in with...')
+			name: 'Credentials',
+			// The credentials is used to generate a suitable form on the sign in page.
+			// You can specify whatever fields you are expecting to be submitted.
+			// e.g. domain, username, password, 2FA token, etc.
+			// You can pass any HTML attribute to the <input> tag through the object.
+			credentials: {
+				username: {
+					label: 'Email',
+					type: 'email',
+					placeholder: 'Email',
+				},
+				password: {
+					label: 'Password',
+					type: 'password',
+					placeholder: 'Password',
+				},
+			},
+			async authorize(credentials, req) {
+				const user = await prisma.user.findFirst({
+					where: {
+						email: credentials.email,
+						password: credentials.password,
+					},
+				});
+
+				// If no error and we have user data, return it
+				if (user) {
+					return user;
+				}
+				// Return null if user data could not be retrieved
+				return null;
+			},
+		}),
+	],
+	callbacks: {
+		jwt: async ({ token, user }) => {
+			if (typeof user !== typeof undefined) token.user = user;
+
+			return token;
+		},
+		session: async ({ session, token }) => {
+			token?.user && (session.user = token.user);
+
+			return session;
+		},
+	},
+};
+
+export default NextAuth(authOptions);
