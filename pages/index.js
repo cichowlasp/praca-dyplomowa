a3bee3e510ca0d45b1aaff2bbab2e0c7cbfdab80
Piotr Cichowlas <cichowlas.p@gmail.com>
2022-10-24 12:08:10 +0200

    updated gitignore

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,13 @@ import Nav from '../components/Navbar';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import { useSession } from 'next-auth/react';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const Home = () => {
-	const { data } = useSession();
-
+	const router = useRouter();
+	const { data, status } = useSession();
+	console.log(status);
 	return (
 		<div className={styles.container}>
 			<Nav />
@@ -22,10 +24,7 @@ const Home = () => {
 
 			<main className={styles.main}>
 				<h1 className={styles.title}>
-					Welcome{' '}
-					<a href='#'>
-						{data?.user?.name + ' ' + data?.user?.surname}
-					</a>
+					Home <a href='#'>Page</a>
 				</h1>
 
 				<p className={styles.description}>
