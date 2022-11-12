3e4853b578bc40dc93d692301d106630368b1911
cichowlasp <cichowlas.p@gmail.com>
2022-11-12 18:44:07 +0100

    added search to users in admin page

--- components/AdminUsers.tsx
+++ components/AdminUsers.tsx
@@ -1,15 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserCard from './UserCard';
 import styles from '../styles/AdminUsers.module.css';
+import { TextField } from '@mui/material';
 
 type Props = { data: any[] | null };
 
 const AdminUsers = ({ data }: Props) => {
+	const [searchInputValue, setSearchInputValue] = useState('');
+	const defaultFilters = {
+		search: () => true,
+	};
+	const [filters, setFilters] = useState<{
+		search: (o: any) => boolean;
+	}>(defaultFilters);
+
 	return (
 		<>
-			<h1>Users</h1>
+			<div
+				style={{
+					display: 'grid',
+					gridTemplateColumns: '[first] 1fr',
+					gridGap: '5px',
+					marginTop: '10px',
+					marginBottom: '5px',
+					width: '20rem',
+				}}>
+				<span>
+					<TextField
+						style={{ borderRadius: '50%' }}
+						placeholder='Search'
+						size='small'
+						fullWidth={true}
+						value={searchInputValue}
+						onChange={(event) => {
+							setSearchInputValue(event.target.value);
+							setFilters((prev) => {
+								return {
+									...prev,
+									search: (user) => {
+										return `${
+											user.email + user.company + user.pin
+										}`
+											.toLowerCase()
+											.includes(
+												event.target.value
+													.trim()
+													.toLowerCase()
+											);
+									},
+								};
+							});
+						}}>
+						Search
+					</TextField>
+				</span>
+			</div>
 			<div className={styles.orders}>
-				{data?.map((el, index) => (
+				{data?.filter(filters.search).map((el, index) => (
 					<UserCard key={index} user={el} index={index} />
 				))}
 			</div>
