753ead1c6e34b26d24daa340597c0f422deb1895
cichowlasp <cichowlas.p@gmail.com>
2022-12-10 17:05:41 +0100

    fixed order updating

--- components/AdminOrders.tsx
+++ components/AdminOrders.tsx
@@ -101,9 +101,43 @@ const AdminOrders = () => {
 			localLoading(true);
 			await fetch('/api/admin/getalldata')
 				.then((response) => response.json())
-				.then((data) => {
-					setCompanies(data);
-				});
+				.then(
+					(
+						data: (Company & {
+							users: (User & {
+								orders: (Order & {
+									informations: Info[];
+									messages: Message[];
+								})[];
+							})[];
+						})[]
+					) => {
+						setCompanies(data);
+						let orders: (Order & {
+							informations: Info[];
+							messages: Message[];
+							user: User;
+						})[] = [];
+						data.forEach((el) => {
+							el.users.forEach((el) => {
+								const fixedOrders = el.orders.map((order) => {
+									return { ...order, user: el };
+								});
+								orders = [...orders, ...fixedOrders];
+							});
+						});
+						orders.sort((a, b) => {
+							if (a.creationData > b.creationData) {
+								return -1;
+							}
+							if (a.creationData < b.creationData) {
+								return 1;
+							}
+							return 0;
+						});
+						setOrders(orders);
+					}
+				);
 			localLoading(false);
 			return;
 		}
